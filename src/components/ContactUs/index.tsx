aa296374eff9cc241ec60bb746b85096c90db081
Lucasfield3 <lucasclic@yahho.com.br>
2022-01-19 20:01:28 -0300

    section contact

--- src/components/ContactUs/index.tsx
+++ src/components/ContactUs/index.tsx
@@ -4,7 +4,8 @@ import dogPhoto from '../../../public/images/photos/golden.jpg'
 import cx from 'classnames';
 
 import Image  from 'next/image'
-import { MutableRefObject } from 'react';
+import { CSSProperties, MutableRefObject, useContext } from 'react';
+import { ChangeThemesContext } from '../../context/ChangeThemes';
 
 type Props = {
 
@@ -12,16 +13,80 @@ type Props = {
 
 export const ContactUs = ({}:Props)=>{
 
+    const {changeColor, theme} = useContext(ChangeThemesContext)
+
+    const myStyle = {
+        myBackground:{
+            background:changeColor(
+                'linear-gradient(180deg, #A675B4 64%, #B14D92 100%)',
+                'linear-gradient(180deg, #A675B4 0%, #FFFFFF 100%)',
+                'linear-gradient(180deg, #32282E 0%, #2E2728 0.01%, #722C8D 100%)',
+                'var(--purple)',
+                'linear-gradient(180deg, #FFFFFF 0%, #FFEFEF 6.8%, #FD0D16 100%)',
+                '#FFFFFF',
+                'var(--red)',
+                'linear-gradient(180deg, #A577B4 0%, #FA3944 100%)'   
+            ) 
+        }as CSSProperties,
+        myButton:{
+            background:changeColor(
+                'var(--purple)',
+                'var(--purple)',
+                'var(--purple)',
+                'var(--purple)',
+                'var(--red)',
+                'var(--red)',
+                'white',
+                'white'
+            ), 
+            color:changeColor(
+                'white',
+                'white',
+                'white',
+                'white',
+                'white',
+                'white',
+                'var(--red)',
+                'var(--red)'
+            )
+        } as CSSProperties,
+        myInputs:{
+            border:changeColor(
+                'none',
+                '1px solid #2E2727',
+                'none',
+                'none',
+                '1px solid #2E2727',
+                '1px solid #2E2727',
+                'none', 
+                'none'
+            )
+        }as CSSProperties,
+        myTitle:{
+            color:changeColor(
+                'white', 
+                'var(--black)',
+                'white',
+                'white',
+                'var(--black)',
+                'var(--black)',
+                'white',
+                'white'
+            )   
+        } as CSSProperties
+    }
+
+   
       
     return(
-        <div className={styles.container}>
-            <h1>Contato</h1>
-            <div className={styles.form}>
-                <input placeholder='Seu nome' type="text"/>
-                <input placeholder='Seu email' type="text"/>
-                <input placeholder='Seu telefone' type="text"/>
-                <textarea placeholder='Sua mensagem'/>
-                <button type="submit">Enviar</button>
+        <div style={myStyle.myBackground} className={styles.container}>
+            <h1 style={myStyle.myTitle}>Contato</h1>
+            <div id='list' className={styles.form}>
+                <input style={myStyle.myInputs} placeholder='Seu nome' type="text"/>
+                <input style={myStyle.myInputs} placeholder='Seu email' type="text"/>
+                <input style={myStyle.myInputs} placeholder='Seu telefone' type="text"/>
+                <textarea style={myStyle.myInputs} placeholder='Sua mensagem'/>
+                <button style={myStyle.myButton} type="submit">Enviar</button>
             </div>
         </div>
     )
--- src/components/PhotoGallery/index.tsx
+++ src/components/PhotoGallery/index.tsx
@@ -4,7 +4,8 @@ import dogPhoto from '../../../public/images/photos/golden.jpg'
 import cx from 'classnames';
 
 import Image  from 'next/image'
-import { MutableRefObject } from 'react';
+import { CSSProperties,  useContext, useEffect} from 'react';
+import { ChangeThemesContext } from '../../context/ChangeThemes';
 
 type Props = {
 
@@ -12,15 +13,33 @@ type Props = {
 
 export const PhotoGallery = ({}:Props)=>{
 
+    const {changeColor, theme} = useContext(ChangeThemesContext)
+
     const breakPoints = [
         {width: 1, itemsToShow:1}
       ]
-      
+
+    const myStyle = {
+        myBackground: {
+            background: changeColor(
+                'linear-gradient(180deg, #b85193 0%, #A578B6 100%)',
+                'linear-gradient(180deg, #75308F 0%, #A578B6 100%)',
+                'linear-gradient(180deg, #712C8B 0%, #2E2727 100%)',
+                'var(--black)',
+                'linear-gradient(180deg, #FE0F19 0%, rgba(255, 255, 255, 0.56) 100%)',
+                'var(--red)',
+                'white',
+                'linear-gradient(180deg, #FA3943 0%, #A577B4 100%)'
+            ) 
+        } as CSSProperties
+    }
+
+    
     return(
-        <div className={styles.container}>
-            <h1>Nossa galeria de fotos</h1>
+        <div style={myStyle.myBackground} className={styles.container}>
+            <h1 style={{color:theme === 'branco-vermelho-roxo' ? 'var(--black)' : 'white'}}>Nossa galeria de fotos</h1>
              <Carousel isRTL={false} breakPoints={breakPoints}>
-                <div className={styles.card}>
+                <div style={{border:theme === 'branco-vermelho-roxo' ? '1px solid' : 'none'}} className={styles.card}>
                     <Image height="250" width="250" src={dogPhoto} alt='foto'/>
                     <p>Golden retriver brincando depois de uma sessão. Texto descritivo.</p>
                 </div>
